73e45731c6f6faa55508251dd071466427434591
Christian Aichner <contact@aichner-christian.com>
2020-05-25 22:14:18 +0200

    Improve license
    The copyright year has been fixed.

--- src/components/atoms/ScrollToTop/index.jsx
+++ src/components/atoms/ScrollToTop/index.jsx
@@ -31,5 +31,5 @@ export default withRouter(ScrollToTop);
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
- * Copyright © 2019-2020 Werbeagentur Christian Aichner
+ * Copyright © 2020 Werbeagentur Christian Aichner
  */
